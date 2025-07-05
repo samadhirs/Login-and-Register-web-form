52f4cd3ddcfd5e921ea53d18adc13cf3b69a6061
Samadhi Rasanjalee <126862124+samadhirs@users.noreply.github.com>
2025-07-06 02:06:54 +0530

    backend: connect.php created

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="login";
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error)
+{
+    echo "Failed to connect DataBase".$conn->connect_error;
+}
+
+
+?>
